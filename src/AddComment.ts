c1f99e3a22b68e19ade98150bb15e57d850bab43
Aneesh <aneesh.chan@gmail.com>
2019-10-28 22:15:39 -0500

    Clean-up

--- src/AddComment.ts
+++ src/AddComment.ts
@@ -4,115 +4,109 @@ import {GraphQlQueryResponseData} from "@octokit/graphql/dist-types/types";
 
 
 class AddComment {
-    token:string = '0e325384c4990d888a3a535eb1ba76611cfd5cc3';
 
-  private getOwnerAndRepo(): string[] {
-      console.log(`process.env.GITHUB_REPOSITORY ${process.env.GITHUB_REPOSITORY}` )
-    if (process.env.GITHUB_REPOSITORY) {
-      return process.env.GITHUB_REPOSITORY.split('/');
 
-    } else {
-        console.log("Error in getOwnerAndRepo");
-      throw new Error('not able to obtain GITHUB_REPOSITORY from process.env');
+    private getOwnerAndRepo(): string[] {
+        console.log(`process.env.GITHUB_REPOSITORY ${process.env.GITHUB_REPOSITORY}`)
+        if (process.env.GITHUB_REPOSITORY) {
+            return process.env.GITHUB_REPOSITORY.split('/');
+
+        } else {
+            console.log('Error in getOwnerAndRepo');
+            throw new Error('not able to obtain GITHUB_REPOSITORY from process.env');
+        }
+
     }
 
-  }
 
-
-  private addPullRequestCommentMutation(): string{
-    return `mutation AddPullRequestComment($subjectId: ID!, $body: String!) {
-  addComment(input:{subjectId:$subjectId, body: $body}) {
-    commentEdge {
-        node {
-        createdAt
-        body
-      }
-    }
-    subject {
-      id
-    }
-  }
-}`;
-  }
-
-
-  private getPullNumber(): number {
-      console.log(`process.env.GITHUB_REF ${process.env.GITHUB_REF}`);
-      return 1;
-    if (process.env.GITHUB_REF) {
-        return 1;
-      // return parseInt(process.env.GITHUB_REF.split('/')[2]);
-    }else {
-      throw new Error('GITHUB_REF is missing in process.env');
+    private addPullRequestCommentMutation(): string {
+        return `mutation AddPullRequestComment($subjectId: ID!, $body: String!) {
+                addComment(input:{subjectId:$subjectId, body: $body}) {
+                    commentEdge {
+                        node {
+                            createdAt
+                            body
+                        }
+                    }
+                    subject {
+                        id
+                    }
+                }
+            }`;
     }
 
-  }
 
+    private getPullNumber(): number {
+        if (process.env.GITHUB_REF) {
 
-  private findPullRequestQuery(): string  {
-    return `query FindPullRequestID ($owner: String!, $repo: String!, $pullNumber: Int!){
-  repository(owner:$owner, name:$repo) {
-    pullRequest(number:$pullNumber) {
-      id
+            return parseInt(process.env.GITHUB_REF.split('/')[2]);
+        } else {
+            throw new Error('GITHUB_REF is missing in process.env');
+        }
+
     }
-  }
-}`;
-  }
-
-  private async addCommentUsingSubjectId(pullRequestId: GraphQlQueryResponseData, comment: string) {
-      console.log(`pullRequestId  ===>>>> ${pullRequestId}`)
-    let data = JSON.parse(JSON.stringify(pullRequestId));
-      console.log(`Parsed pull request id ${data}`)
-    // const token = core.getInput('repo-token');
-      console.log(`repo-token ${this.token.length}`);
-    let graphQlResponse = graphql(this.addPullRequestCommentMutation(), {
-          headers: {
-            authorization: `token 1e3754d26a24ee115c50f37bd9267abde193ba0f`,
-          },
-          subjectId: data.repository.pullRequest.id,
-          body: comment,
-
-        },
-    );
-    console.log(`Adding the comment ...`);
-    return await graphQlResponse;
-
-  }
 
 
-
-
-  private async getSubjectId(findPullRequestIdQuery: string, nameAndRepo: string[]) {
-      console.log('Inside getSubjectId');
-    // const token = core.getInput('repo-token');
-      console.log(`repo-token ${this.token}`);
-    let newVar: GraphQlQueryResponseData = await graphql(findPullRequestIdQuery, {
-          headers: {
-            authorization: `token 1e3754d26a24ee115c50f37bd9267abde193ba0f`,
-          },
-          owner: nameAndRepo[0],
-          repo: nameAndRepo[1],
-          pullNumber: this.getPullNumber(),
-        },
-    );
-    console.log(`Exiting getSubject Id` );
-    return newVar;
-
-  }
-
-  async addComment(comment: string) {
-      console.log('Inside addComment');
-    const nameAndRepo: string[] = this.getOwnerAndRepo();
-    const [name, repo] = nameAndRepo
-    console.log(`Name is ${name}  and repo is ${repo}`);
-    const findPullRequestIdQuery = this.findPullRequestQuery();
-    try {
-      const subjectId = await this.getSubjectId(findPullRequestIdQuery, nameAndRepo);
-      return await this.addCommentUsingSubjectId(subjectId, comment);
-    } catch (e) {
-        console.error(e);
+    private findPullRequestQuery(): string {
+        return `query FindPullRequestID ($owner: String!, $repo: String!, $pullNumber: Int!){
+                repository(owner:$owner, name:$repo) {
+                    pullRequest(number:$pullNumber) {
+                        id
+                    }
+                }
+            }`;
     }
-  }
+
+    private async addCommentUsingSubjectId(pullRequestId: GraphQlQueryResponseData, comment: string) {
+        console.log(`pullRequestId  ===>>>> ${pullRequestId}`)
+        let data = JSON.parse(JSON.stringify(pullRequestId));
+        console.log(`Parsed pull request id ${data}`)
+        const token = core.getInput('repo-token');
+        let graphQlResponse = graphql(this.addPullRequestCommentMutation(), {
+                headers: {
+                    authorization: `token ${token}`,
+                },
+                subjectId: data.repository.pullRequest.id,
+                body: comment,
+
+            },
+        );
+        console.log(`Adding the comment ...`);
+        return await graphQlResponse;
+
+    }
+
+
+    private async getSubjectId(findPullRequestIdQuery: string, nameAndRepo: string[]) {
+        console.log('Inside getSubjectId');
+        const token = core.getInput('repo-token');
+        let newVar: GraphQlQueryResponseData = await graphql(findPullRequestIdQuery, {
+                headers: {
+                    authorization: `token ${token}`,
+                },
+                owner: nameAndRepo[0],
+                repo: nameAndRepo[1],
+                pullNumber: this.getPullNumber(),
+            },
+        );
+        console.log(`Exiting getSubject Id`);
+        return newVar;
+
+    }
+
+    async addComment(comment: string) {
+        console.log('Inside addComment');
+        const nameAndRepo: string[] = this.getOwnerAndRepo();
+        const [name, repo] = nameAndRepo
+        console.log(`Name is ${name}  and repo is ${repo}`);
+        const findPullRequestIdQuery = this.findPullRequestQuery();
+        try {
+            const subjectId = await this.getSubjectId(findPullRequestIdQuery, nameAndRepo);
+            return await this.addCommentUsingSubjectId(subjectId, comment);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
 
 export default AddComment;
